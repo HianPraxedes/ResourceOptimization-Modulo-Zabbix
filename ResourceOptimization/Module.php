c73b5736e3d0556fa2662b0b1adaf73bcf71b018
Hian Praxedes <78980856+HianPraxedes@users.noreply.github.com>
2025-02-21 09:37:10 -0300

    Adicionando restrição de visualização para somente para cargos de Admin ou superior

--- ResourceOptimization/Module.php
+++ ResourceOptimization/Module.php
@@ -1,19 +1,22 @@
 <?php
-       
-       namespace Modules\ResourceOptimization;
-       
-       use Zabbix\Core\CModule,
-           APP,
-           CMenuItem;
-       
-       class Module extends CModule {
-       
-           public function init(): void {
-               APP::Component()->get('menu.main')
-                   ->findOrAdd(_('Monitoring'))
-                   ->getSubmenu()
-                   ->insertAfter(_('Discovery'),
-                       (new CMenuItem(_('Resource Optimization')))->setAction('resource.optimization')
-                   );
-           }
-       }
+namespace Modules\ResourceOptimization;
+
+use Zabbix\Core\CModule,
+    APP,
+    CMenuItem,
+    CWebUser;
+
+class Module extends CModule {
+
+    public function init(): void {
+        // Adiciona o menu para usuários com nível de privilégio igual ou superior a admin.
+        if (isset(CWebUser::$data) && CWebUser::$data['type'] >= USER_TYPE_ZABBIX_ADMIN) {
+            APP::Component()->get('menu.main')
+                ->findOrAdd(_('Monitoring'))
+                ->getSubmenu()
+                ->insertAfter(_('Discovery'),
+                    (new CMenuItem(_('Resource Optimization')))->setAction('resource.optimization')
+                );
+        }
+    }
+}
