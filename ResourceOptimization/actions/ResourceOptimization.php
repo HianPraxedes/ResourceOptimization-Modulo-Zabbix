6da08de01998efc5c8dc1ce4e7b5ad6bb68d5771
Hian Praxedes <78980856+HianPraxedes@users.noreply.github.com>
2025-03-07 10:52:42 -0300

    Mudando para 8 horas

--- ResourceOptimization/actions/ResourceOptimization.php
+++ ResourceOptimization/actions/ResourceOptimization.php
@@ -43,7 +43,7 @@ class ResourceOptimization extends CController {
 
         // Define o caminho do arquivo de cache e o tempo de expiração (em segundos)
         $cacheFile = '/tmp/resource_optimization_cache.json';
-        $cacheTime = 10; // 10 segundos para testes (substitua por 28800 para 8 horas)
+        $cacheTime = 28800; // 10 segundos para testes (substitua por 28800 para 8 horas)
 
         // Se o cache existir e estiver válido, utiliza os dados do cache
         if (file_exists($cacheFile) && (time() - filemtime($cacheFile) < $cacheTime)) {
